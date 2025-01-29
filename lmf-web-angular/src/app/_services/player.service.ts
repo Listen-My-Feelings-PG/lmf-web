61acd73324badc0064225a8e3379bbe10875c3cd
foxfrancis005 <j7_samuslive.dr@hotmail.com>
2025-01-29 12:43:16 -0600

    inicio de refactorización del reproductor

---
+++ lmf-web-angular/src/app/_services/player.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerService {
+  //El servicio controlará la lista de reproducción.
+  constructor() { }
+}
