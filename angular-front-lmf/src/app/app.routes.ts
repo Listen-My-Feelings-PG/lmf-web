aebc1d7af92e994774501db7bd06c1677d544845
FOX04 <J7_samuslive.dr@hotmail.com>
2026-02-21 12:58:05 -0600

    feat: add main and menu components with routing
    - Created MainComponent and MenuComponent with their respective HTML, SCSS, and spec files.
    - Updated app.routes.ts to include routing for the main and menu components.
    - Removed placeholder content from app.component.html.

--- angular-front-lmf/src/app/app.routes.ts
+++ angular-front-lmf/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { MenuComponent } from './main/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'main', component: MainComponent, children: [
+      { path: 'menu', component: MenuComponent },
+      { path: '', redirectTo: 'menu', pathMatch: 'full' }
+    ]
+  },
+  { path: '', redirectTo: 'main', pathMatch: 'full' }
+];
