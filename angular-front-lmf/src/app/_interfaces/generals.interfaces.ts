417d7aa5887ebb862c36b5335899f2c8957d3470
FOX04 <J7_samuslive.dr@hotmail.com>
2026-02-21 14:06:33 -0600

    feat: add decimal.js dependency and create Song, Playlist, and TsModel classes

---
+++ angular-front-lmf/src/app/_interfaces/generals.interfaces.ts
@@ -0,0 +1,6 @@
+import { Decimal } from 'decimal.js';
+export interface LibrosaTsFeatures {
+  mel_spectrogram: Array<Array<number>>,
+  tempo: number
+}
+
---
+++ angular-front-lmf/src/app/_models/generals.models.ts
@@ -0,0 +1,39 @@
+export class Song {
+  public id?: number;
+  public fileName!: string;
+  public userScore!: number;
+  public tsScore: number = 0;
+  public tsTrainLevelLocal: number = 0;
+  public tsTrainLevelGlobal: number = 0;
+  public tsStatusLocal: 'trained' | 'error' | null = null;
+  public tsStatusGlobal: 'trained' | 'error' | null = null;
+  public metadata?: {
+    title: string,
+    artist: string,
+    album: string,
+    urlAlbumArt: string
+  };
+  constructor(data: Partial<Song> = {}) {
+    Object.assign(this, data);
+  }
+}
+
+export class Playlist {
+  constructor(
+    public name: string,
+    public songs: Array<Song>,
+    public model: TsModel | null,
+    public isGlobal: boolean,
+    public id?: number
+  ) { }
+}
+
+export class TsModel {
+  constructor(
+    public id: number,
+    public type: 'tensorflow',
+    public global: boolean,
+    public trainCount: number,
+    public data: any
+  ) { }
+}
